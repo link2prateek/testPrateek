de47473a5d77ec1232cf062c7f875b1228c03174
prateek <link2prateekkumar@gmail.com>
2015-03-08 13:17:36 +0530

    initial

---
+++ src/main/java/com/prateek/model/BankAccountService.java
@@ -0,0 +1,14 @@
+package com.prateek.model;
+
+import javax.jws.WebService;
+
+
+@WebService
+public interface BankAccountService {
+	
+	
+	
+	
+	
+
+}
---
+++ src/main/java/com/prateek/model/UserDetails.java
@@ -0,0 +1,30 @@
+package com.prateek.model;
+
+import java.math.BigDecimal;
+
+public class UserDetails {
+	
+	private String userName;
+	private BigDecimal accountBalance;
+	
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public BigDecimal getAccountBalance() {
+		return accountBalance;
+	}
+
+	public void setAccountBalance(BigDecimal accountBalance) {
+		this.accountBalance = accountBalance;
+	}
+
+	
+	
+	
+
+}
